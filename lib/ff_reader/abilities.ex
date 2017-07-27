01359ed958631b770f92a3523246f9adf71fd9e1
Kevin Rocker <kevin.j.rocker@gmail.com>
2017-07-26 23:35:34 -0400

    Authorization for Series/Categories

--- lib/ff_reader/abilities.ex
+++ lib/ff_reader/abilities.ex
@@ -1,10 +1,14 @@
 defimpl Canada.Can, for: FfReader.Accounts.User do
   alias FfReader.Accounts.User
-  alias FfReader.Fiction.{Story}
+  alias FfReader.Fiction.{Story, Category, Series}
 
   def can?(%User{ id: user_id }, _, %User{ id: user_id }), do: true
 
   def can?(%User{ id: user_id }, _, %Story{ author_id: user_id }), do: true
+
+  def can?(%User{ admin: true }, _ , %Category{}), do: true
+
+  def can?(%User{ admin: true }, _, %Series{}), do: true
 
   def can?(%User{ id: user_id }, _, _), do: false
 end
